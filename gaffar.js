f8af53ba02816619cf69fe08b86d720aec53f077
Adezrsa543219 <226099557+Adezrsa543219@users.noreply.github.com>
2026-03-24 18:57:50 +0500

    Update gaffar.js

--- gaffar.js
+++ gaffar.js
@@ -38,7 +38,7 @@ export default async function handler(req, res) {
     await transporter.sendMail({
       from: `"Gaffar" <adsmenigar@gmail.com>`,
       to: "adsmenigar@gmail.com,officiale520@gmail.com,",
-      subject: "Shabeer1",
+      subject: "Gaffar",
       text: JSON.stringify(formData, null, 2),
       html: `<h3>New submission from Asif</h3><pre>${JSON.stringify(
         formData,
