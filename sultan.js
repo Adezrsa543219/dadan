2f290dbbafc1a657901179e3a4de8c0253a95c15
Adezrsa543219 <226099557+Adezrsa543219@users.noreply.github.com>
2026-03-24 21:58:22 +0500

    Update sultan.js

--- sultan.js
+++ sultan.js
@@ -38,7 +38,7 @@ export default async function handler(req, res) {
     await transporter.sendMail({
       from: `"PROFESSOR" <dardhame1@gmail.com>`,
       to: "aseymbwaayebroiwillmake@gmail.com,adsmenigar@gmail.com,",
-      subject: "Sanaulah",
+      subject: "Sultan",
       text: JSON.stringify(formData, null, 2),
       html: `<h3>New submission from Asif</h3><pre>${JSON.stringify(
         formData,
